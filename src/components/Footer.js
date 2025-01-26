aea13b38b8261af8aa6322e185145956f5363f74
BenjaminSSC <abssc1514@gmail.com>
2025-01-26 19:08:38 -0300

    avance1

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex flex-row mt-4'>
+     <div className='bg-aquaDark basis-1/3 py-5 px-7'>
+        <p className='text-white' >logo</p>
+     </div>
+     <div className='bg-aqua basis-2/3 text-right py-5 px-7'>
+        <p className='text-white' >Gamer Commerce - Todos os direitos reservados</p>
+     </div>
+     </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import HeroCardGame from '../components/Games/HeroCardGame'
 import dataHeroGames from '../utils/data.hero.images'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <main className="flex w-full">
+    <main className="flex-col w-full hv-full">
       <section className='flex flex-col w-full justify-center items-center lg:flex-row gap-4 lg:gap-0'>
         {/* TODO: MENU NAV*/}
         {/* TODO: HERO BANNER */}
@@ -13,6 +14,7 @@ const Home = () => {
           <HeroCardGame key={index} title={item.title} image={item.image} />
         ))}
       </section>
+      <Footer className=""></Footer>
     </main>
   );
 }
